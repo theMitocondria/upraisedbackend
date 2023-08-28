08e8b3f6678ddeb7bf5ca0a4bffe66be11d88157
gautam khatri <buddhgautam777@gmail.com>
2023-08-28 11:01:33 +0530

    registration done

--- app.js
+++ app.js
@@ -1,99 +1,73 @@
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDatabase from './database.js';
-// import user from './routes/user.js';
-
-// const app = express();
-
-// // configuring the dotenv file so that we can access the envirmental vairables
-// dotenv.config();
-
-// // using the cors , to avoid getting the cors errors in frontend
-// app.use(cors());
-
-// //making the function call to connect to databse;
-// connectDatabase();
+import user from './routes/user.js';
+import session from "express-session";
+import { Strategy } from "passport-google-oauth20";
+import passport from "passport";
 
 
-// //running the middlares
-// app.use(express.static("public"));
-// app.use(express.json());
-
-
-
-// app.use("/auth", user);
-
-
-// app.listen(process.env.PORT, () => {
-//     console.log(`port is running at ${process.env.PORT}`)
-// })
-
-
-
-import express from "express";
-import dotenv from "dotenv";
-// import connectDatabase from "./Databse.js";
-import {Strategy} from 'passport-google-oauth20';
-import passport from 'passport';
-import session from 'express-session';
-// import axios from 'axios';
 const app = express();
+
+// configuring the dotenv file so that we can access the envirmental vairables
+dotenv.config();
+
+//making the function call to connect to databse;
 connectDatabase();
 
-// env consts
-dotenv.config();
+// using the cors , to avoid getting the cors errors in frontend
+app.use(cors());
+
+
+
+
+//running the middlares
+app.use(express.static("public"));
+app.use(express.json());
+app.use(
+    session({
+      secret: "Enter your secret key",
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
+
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+
+
 const port = process.env.PORT;
 
-app.use(session({ 
-    secret: 'Enter your secret key',
-    resave: true,
-    saveUninitialized: true
-}))
 
 
-// GOOGLE OAUTH
-// this is a new comment
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function (user, cb) {
-    cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-    cb(null, obj);
-});
-
-passport.use(new Strategy({
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: 'http://localhost:3000/auth/google/callback'
-    },
-    function (accessToken, refreshToken, profile, done) {
-        // if user already exist in your dataabse login otherwis
-        // console.log({"p":profile})
-        // save data and signup
-        done(null, {profile});
-    }
-));
+app.use("/auth", user);
 
 
-app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
-
-app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/auth/fail'}),
-    (req, res, next) => {
-        console.log(req.user, req.isAuthenticated());
-        res.send('user is logged in');
-    })
-
-app.get('/auth/fail', (req, res, next) => {
-    res.send('user logged in failed');
-});
 
 
-// app endpoint listen
-app.listen(port,()=>{
-    console.log(`Connection made on ${port} port`);
+// app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+
+// app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/auth/fail'}),
+//     (req, res, next) => {
+//         console.log(req.user, req.isAuthenticated());
+       
+//         res.send('user is logged in');
+//     })
+
+// app.get('/auth/fail', (req, res, next) => {
+//     res.send('user logged in failed');
+// });
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`port is running at ${process.env.PORT}`)
 })
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,65 @@
-import express from 'express';
-import { login, register, verifyOtp } from '../controllers/user.js';
+import express from "express";
+import { Strategy } from "passport-google-oauth20";
+import passport from "passport";
 
+import dotenv from "dotenv";
+import { forgetPassword, login, register, resetPassword, signInWithGoogle, verifyOtp } from "../controllers/user.js";
+import User from "../models/User.js";
+
+
+dotenv.config();
 const userRouter = express.Router();
 
-userRouter.post("/register", register)
-userRouter.post("/verify-user", verifyOtp)
-userRouter.post("/login", login)
+
+// GOOGLE OAUTH
+// this is a new comment
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+passport.use(
+  new Strategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+   async function (accessToken, refreshToken, profile, done) {
+      // console.log(accessToken);
+      // const user = await User.findOne({accessToken});
+      // if(!user){
+      //   signInWithGoogle;
+      // }
+      done(null, { profile });
+    }
+  )
+);
+
+userRouter.post("/register", register);
+userRouter.post("/verify-user", verifyOtp);
+userRouter.post("/login", login);
+
+userRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+userRouter.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/auth/fail" }),
+  signInWithGoogle
+);
+
+userRouter.get("/fail", (req, res, next) => {
+  res.send("user logged in failed");
+});
+
+userRouter.post("/forget/password", forgetPassword);
+userRouter.post("/reset/password", resetPassword);
 
 export default userRouter;
